df57c863ed5ce6621ba259a7a42264bd8135d081
Erik Rodhe <erikrodhe@gmail.com>
2019-05-20 15:27:19 +0200

    Add CORS dev headers

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,5 @@
 from sanic import Sanic
-from sanic.response import json
-
+from sanic import response
 
 #LOGGING_CONFIG='debug'
 #app = Sanic('test', logging=LOGGING_CONFIG)
@@ -9,7 +8,12 @@ app.config.from_envvar('MYAPP_SETTINGS')
 
 @app.route("/")
 async def test(request):
-    return json({"hello": "world"})
+    return response.json(
+            {"hello": "world"},
+            headers={"Access-Control-Allow-Origin": "http://localhost:8080"},
+            content_type="application/json",
+            status=200
+            )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
